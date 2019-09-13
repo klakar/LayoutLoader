95ac0219e2a7b6ec0715979dd402c26339b3701c
Klas Karlsson <klakar@users.noreply.github.com>
2019-09-13 19:32:03 +0200

    Adding UTM grid funktion for layouts

--- profile/python/expressions/mgrsfunctions.py
+++ profile/python/expressions/mgrsfunctions.py
@@ -10,6 +10,36 @@ from qgis.gui import *
 Functions to be used with grid coordinates (mostly)
 """
 
+def index_number(number_string):
+    """
+    Function that converts all input numbers in string format to unicode superscript.
+    """
+    numbers = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
+    try:
+        return_string = ''
+        for n in number_string:
+            return_string += numbers[int(n)]
+    except:
+        return_string = 'error'
+    return return_string
+
+@qgsfunction(args='auto', group='Military')
+def utm_grid(grid_number, feature, parent):
+    """
+    <h2>Example:</h2><br>
+    Creates UTM kilometer numbers from grid number variables.<br>
+    Even 10 km numbers will have 100 km index numbers.<br>
+    (Requires font with unicode support)<br><br>
+    <code>
+    utm_grid(@grid_number) -> '46' alt '⁵40'<br>
+    </code>
+    """
+    grid_string = str(int(grid_number))
+    km_string = ''
+    if grid_string[-4:-3] == '0':
+        km_string = index_number(grid_string[:-5])
+    km_string += grid_string[-5:-3]
+    return km_string
 
 @qgsfunction(args='auto', group='Military') 
 def mgrs(easting, northing, epsg, feature, parent): 
